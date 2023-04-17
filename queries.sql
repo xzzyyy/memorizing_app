abc70f7e6ea7ed4c57640b6add98c69ca2c7f599
Pavel S <xzzyyy1@gmail.com>
2023-04-17 23:19:19 +0100

    some changes in .sql queries

--- queries.sql
+++ queries.sql
@@ -1,5 +1,4 @@
 DROP TABLE qa_stats;
-
 CREATE TABLE qa_stats (
     id TEXT NOT NULL PRIMARY KEY,
     qa TEXT NOT NULL,
@@ -7,17 +6,17 @@ CREATE TABLE qa_stats (
     correct INTEGER NOT NULL,
     wrong INTEGER NOT NULL
 );
-
 CREATE TABLE state (
     key TEXT NOT NULL PRIMARY KEY,
     value TEXT NOT NULL
 );
-INSERT INTO state VALUES ('last_qa_id', '');
 
+INSERT INTO state VALUES ('last_qa_id', '');
 INSERT INTO qa VALUES ('qa2', 'single quotes', 0, 0);
 INSERT INTO qa VALUES ('qa3', "double quotes", 0, 0);
 INSERT INTO qa VALUES ("qa4", 'single quotes', 0, 0);
 
+SELECT COUNT(*) FROM qa_stats;
 SELECT COUNT(*) FROM qa WHERE id IN ("qa1", "qa2");
 
 DELETE FROM qa2
@@ -26,9 +25,9 @@ SELECT id, qa, '', correct, wrong FROM qa_stats;
 INSERT INTO qa2 SELECT id, '', '', correct, wrong FROM qa_stats;
 INSERT INTO qa_stats SELECT * FROM qa2;
 
-SELECT * FROM qa_stats;
-SELECT id, md FROM qa_stats;
-SELECT * FROM qa_stats WHERE id = 'vec_ops';
+SELECT * FROM qa_stats WHERE id = 'code_review';
+DELETE FROM qa_stats WHERE id = 'refactoring';
+SELECT * FROM state;
 
 SELECT SUM(wrong + correct) FROM qa_stats;
 SELECT (SELECT SUM(wrong + correct) FROM qa_stats) AS answered, (SELECT COUNT(*) FROM qa_stats) AS cnt;
